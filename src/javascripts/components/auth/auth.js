1bf5aca82b91dd4f137b1b42f5a1e166f02e091d
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-12 21:19:33 -0500

    added auth.js file with sign me in function

---
+++ src/javascripts/components/auth/auth.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
