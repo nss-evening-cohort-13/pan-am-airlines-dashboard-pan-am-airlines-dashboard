b81ce5bac73d4cd68ed497a6e496b4141682b496
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-15 20:58:14 -0500

    got the add button to display and hide on login/logout.

--- src/javascripts/components/views/foodView.js
+++ src/javascripts/components/views/foodView.js
@@ -2,17 +2,20 @@ import FoodData from '../../helpers/data/foodData';
 import FoodCard from '../cards/foodCard';
 
 const showFood = (user) => {
-  $('#app').append('<div id="food-button-area"></div>');
+  $('#app').append('<div id="food-btn-area" class="button-area"></div>');
   $('#app').append('<div id="food-area"></div>');
-  $('#button-area').append(
-    '<button type="button" class="btn btn-success" id="add-food-btn"><i class="fas fa-plus-circle"></i>Add a Food Item</button>'
-  );
+
   FoodData.getFoodItems().then((response) => {
     if (response.length) {
       $('#food-area').html('');
       response.forEach((foodItem) => {
         $('#food-area').append(FoodCard.foodCardBuilder(foodItem));
         if (user) {
+          if ($('#food-btn-area').is(':empty')) {
+            $('#food-btn-area').append(
+              '<button type="button" class="btn btn-success" id="add-food-btn"><i class="fas fa-plus-circle"></i>Add a Food Item</button>'
+            );
+          }
           $(`#${foodItem.food_id}`).append(
             `<a href="#" class="card-link update-link" id=${foodItem.food_id}>Update Item</a>`
           );
