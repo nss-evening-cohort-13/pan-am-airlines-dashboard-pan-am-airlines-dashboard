14a8ad59c5163b6c04d5be441bcd8b8a92fb64fc
Wanda <wmmcinturff@gmail.com>
2020-10-15 22:23:55 -0500

    crew card built

---
+++ src/javascripts/components/cards/crewCard.js
@@ -0,0 +1,16 @@
+const crewBuilder = (crewObj) => {
+  const domString = `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+   <h5 class="card-title">"PanAm Flight Crew</h5> 
+  </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${crewObj.name}</li>
+      <li class="list-group-item">${crewObj.role}</li>
+      <li class="list-group-item">${crewObj.planeUid}</li>
+   </ul>
+  </div>`;
+
+  return domString;
+};
+
+export default { crewBuilder };
