f4a38f324d8c3461f027606b2d976c09a8e48aa6
Autumn Fotopoulos <acarte94@gmail.com>
2020-10-16 17:47:30 -0500

    fixed logged in view

--- src/javascripts/components/views/baggageView.js
+++ src/javascripts/components/views/baggageView.js
@@ -1,18 +1,31 @@
 import baggageData from '../../helpers/data/baggageData';
 import baggageCard from '../cards/baggageCard';
 
-const showBaggage = () => {
-  $('#app').html('');
-  $('#app').append('<div id="button-area"></div>');
+const showBaggage = (user) => {
+  $('#app').append('<div id="baggage-btn-area" class="button-area"></div>');
   $('#app').append('<div id="baggage-area"></div>');
-  $('#button-area').append('<button type="button" class="btn btn-success" id="add-baggage-btn"><i class="fas fa-plus-circle"></i> Add Baggage</button>');
-  baggageData.getBaggage().then((response) => {
+
+  baggageData.getBaggageItems().then((response) => {
     if (response.length) {
+      $('#baggage-area').html('');
       response.forEach((baggage) => {
-        $('#baggage-area').append(baggageCard.baggageBuilder(baggage));
+        $('#baggage-area').append(baggageCard.baggageCardBuilder(baggage));
+        if (user) {
+          if ($('#baggage-btn-area').is(':empty')) {
+            $('#baggage-btn-area').append(
+              '<button type="button" class="btn btn-success" id="add-food-btn"><i class="fas fa-plus-circle"></i>Add Bagggae</button>'
+            );
+          }
+          $(`#${baggage.id}`).append(
+            `<a href="#" class="card-link update-link" id=${baggage.id}>Update Item</a>`
+          );
+          $(`#${baggage.id}`).append(
+            `<a href="#" class="card-link remove-link" id=${baggage.id}>Remove Item</a>`
+          );
+        }
       });
     } else {
-      $('#baggage-area').append('<h1>No Baggage!</h1>');
+      $('#baggage-area').append('<h1>No Food!</h1>');
     }
   });
 };
