e324f31f34f2f09c432ceac97502f0ae2cabd432
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-15 19:57:50 -0500

    created food data get request.

---
+++ src/javascripts/helpers/data/foodData.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getFoodItems = () => new Promise((resolve, reject) => {
+    axios
+      .get(`${baseUrl}/food.json`)
+      .then((response) => {
+        const foodItems = response.data;
+        const foodItemsList = [];
+        if (foodItems) {
+          Object.keys(foodItems).forEach((foodId) => {
+            airportList.push(foodItems[foodId]);
+          });
+        }
+        resolve(foodItemsList);
+      })
+      .catch((error) => reject(error));
+  });
+  
+  export default {
+    getFoodItems,
+  };
+  
