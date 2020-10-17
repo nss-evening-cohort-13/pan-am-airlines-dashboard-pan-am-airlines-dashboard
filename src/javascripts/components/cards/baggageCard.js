1f3bb48f662cb6506181b4ea073696b1ac18fc79
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-16 21:25:16 -0500

    updated baggage card.

--- src/javascripts/components/cards/baggageCard.js
+++ src/javascripts/components/cards/baggageCard.js
@@ -2,10 +2,10 @@ const baggageBuilder = (baggage) => {
   const domString = `<div class="card" style="width: 18rem;">
       <img src="${baggage.image}" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${baggage.city}</h5>
-        <div class="card-text">Location: ${baggage.state}</div>
+        <h5 class="card-title">Owner: ${baggage.owner}</h5>
+        <div class="card-text">Destination: ${baggage.state}</div>
         <ul class="list-group list-group-flush">
-    <li class="list-group-item">${baggage.name}</li>
+    <li class="list-group-item">Color: ${baggage.color}</li>
   </ul>
       
     <div class="card-body"  id="${baggage.uid}">
