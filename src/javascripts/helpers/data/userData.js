af1353b60916141b161104d59b6b3ac1d816608a
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-10-20 21:47:36 -0500

    fixed 400 error for user

--- src/javascripts/helpers/data/userData.js
+++ src/javascripts/helpers/data/userData.js
@@ -16,6 +16,25 @@ const getAllUsers = () => new Promise((resolve, reject) => {
   }).catch((error) => reject(error));
 });
 
+const checkIfUserExistsInFirebase = (user) => {
+  axios
+    .get(`${baseUrl}/users.json?orderBy="uid"&equalTo="${user.uid}"`)
+    .then((resp) => {
+      if (Object.values(resp.data).length === 0) {
+        axios
+          .post(`${baseUrl}/users.json`, user)
+          .then((response) => {
+            const update = { firebaseKey: response.data.name };
+            axios.patch(`${baseUrl}/users/${response.data.name}.json`, update);
+          })
+          .catch((error) => console.warn(error));
+      }
+
+      window.sessionStorage.setItem('ua', true);
+    })
+    .catch((error) => console.error(error));
+};
+
 const setCurrentUser = (userObj) => {
   const user = {
     image: userObj.photoURL,
@@ -24,7 +43,11 @@ const setCurrentUser = (userObj) => {
     email: userObj.email,
     lastSignInTime: userObj.metadata.lastSignInTime,
   };
+  const loggedIn = window.sessionStorage.getItem('ua');
+  if (!loggedIn) {
+    checkIfUserExistsInFirebase(user);
+  }
   return user;
 };
 
-export default { setCurrentUser, getAllUsers };
+export default { setCurrentUser, getAllUsers, checkIfUserExistsInFirebase };
