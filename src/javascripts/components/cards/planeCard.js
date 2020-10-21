7440d7bc4c6b2901fd9af52efc32724d0a96a960
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-10-21 18:46:46 -0500

    delete plane function is working

--- src/javascripts/components/cards/planeCard.js
+++ src/javascripts/components/cards/planeCard.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import planeData from '../../helpers/data/planeData';
 
 const planeMaker = (plane) => {
   let buttons = '';
   const user = firebase.auth().currentUser;
   if (user) {
-    buttons += `<a href="#" id="${plane.uid}" class="btn btn-info update-plane"><i class="far fa-edit"></i> Update plane</a>
-    <a href="#" id="${plane.uid}" class="btn btn-danger delete-plane">Delete plane</a>
+    buttons += `<a href="#" id="${plane.uid}" class="btn btn-info update-plane"><i class="far fa-edit"></i> Update Plane</a>
+    <a href="#" id="${plane.uid}" class="btn btn-danger delete-plane"><i class="fas fa-trash"></i> Delete Plane</a>
   `;
   }
-  const domString = `<div class="card" style="width: 18rem;">
+  const domString = `<div class="card plane" id="plane-${plane.uid}" style="width: 18rem;">
       <img src="${plane.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${plane.name}</h5>
@@ -19,7 +20,12 @@ const planeMaker = (plane) => {
       </div>
       ${buttons}
     </div>`;
-
+  $('body').on('click', '.delete-plane', (e) => {
+    e.stopImmediatePropagation();
+    const uid = e.currentTarget.id;
+    $(`.card#plane-${uid}`).remove();
+    planeData.deletePlanes(uid);
+  });
   return domString;
 };
 
