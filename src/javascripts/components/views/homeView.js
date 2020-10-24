95c1b51df3e9bf2d37c0c250b20666f51566926e
wkotheimer <wkotheimer@gmail.com>
2020-10-24 10:41:06 -0500

    Adding board below image

--- src/javascripts/components/views/homeView.js
+++ src/javascripts/components/views/homeView.js
@@ -5,7 +5,7 @@ const showHomepage = () => {
   $('#app').html('');
   $('#app').append('<p>Welcome to the PanAm DashBoard!</p>');
   $('#app').append('<div id="landing-page-image"></div>');
-  $('#landing-page-image').append('<div id="landing-page-table"></div>');
+  $('#app').append('<div id="landing-page-table"></div>');
 
   flightData.getFlights().then((response) => {
     if (response.length) {
