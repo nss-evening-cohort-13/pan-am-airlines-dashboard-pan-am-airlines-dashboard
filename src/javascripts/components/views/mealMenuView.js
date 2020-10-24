f8a9f363ab9a6c1831bebe38446000e98f9cf41c
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-10-24 12:53:07 -0500

    meal menu button working

--- src/javascripts/components/views/mealMenuView.js
+++ src/javascripts/components/views/mealMenuView.js
@@ -4,11 +4,18 @@ import FoodCard from '../cards/foodCard';
 const showMeals = (user) => {
   $('#app').html('');
   $('#app').append('<div id="food-btn-area" class="button-area"></div>');
+  $('#app').append('<div id="meal-btn-area" class="button-area"></div>');
   $('#app').append('<div id="meal-area"></div>');
 
   FoodData.getMeals().then((response) => {
     if (response.length) {
       $('#meal-area').html('');
+      $('#meal-btn-area').append(
+        '<button type="button" class="btn btn-dark" id="snack-btn"><i class="fas fa-utensils"></i> Snack Menu</button>'
+      );
+      $('#meal-btn-area').append(
+        '<button type="button" class="btn btn-warning" id="meal-btn"><i class="fas fa-utensils"></i> Meal Menu</button>'
+      );
       response.forEach((foodItem) => {
         if (foodItem !== undefined) {
           $('#meal-area').append(FoodCard.foodCardBuilder(foodItem));
@@ -16,12 +23,6 @@ const showMeals = (user) => {
             if ($('#food-btn-area').is(':empty')) {
               $('#food-btn-area').append(
                 '<button type="button" class="btn btn-success" id="add-food-btn"><i class="fas fa-plus-circle"></i> Add a Food Item</button>'
-              );
-              $('#food-btn-area').append(
-                '<button type="button" class="btn btn-info" id="snack-btn"><i class="fas fa-utensils"></i> Snack Menu</button>'
-              );
-              $('#food-btn-area').append(
-                '<button type="button" class="btn btn-warning" id="meal-btn"><i class="fas fa-utensils"></i> Meal Menu</button>'
               );
             }
           }
