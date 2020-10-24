a112f9c4c0396a4d98ba3fc58d044da53b1f331e
wkotheimer <wkotheimer@gmail.com>
2020-10-24 10:30:47 -0500

    Added flight board

--- src/javascripts/components/views/homeView.js
+++ src/javascripts/components/views/homeView.js
@@ -1,7 +1,36 @@
+import flightData from '../../helpers/data/flightData';
+import flightBoardCard from '../cards/flightBoardCard';
+
 const showHomepage = () => {
   $('#app').html('');
   $('#app').append('<p>Welcome to the PanAm DashBoard!</p>');
   $('#app').append('<div id="landing-page-image"></div>');
+  $('#landing-page-image').append('<div id="landing-page-table"></div>');
+
+  flightData.getFlights().then((response) => {
+    if (response.length) {
+      let rows = '';
+      response.forEach((flight) => {
+        console.warn(flightBoardCard.flightBoardCard(flight));
+        rows += flightBoardCard.flightBoardCard(flight);
+      });
+      $('#landing-page-table').html('');
+      $('#landing-page-table').append(
+        `<div><h1>Departures</h1></div><table class="table table-hover table-responsive table-dark table-striped">
+          <thead>
+            <tr>
+              <th>Flight Number</th>
+              <th>Flight Duration</th>
+              <th>Departure Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${rows}
+          </tbody>
+      </table>`
+      );
+    }
+  });
 };
 
 export default { showHomepage };
