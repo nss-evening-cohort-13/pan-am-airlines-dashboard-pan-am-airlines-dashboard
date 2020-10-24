b60fad5dbc3988037430d7df90b19a5b69567d62
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-24 13:29:02 -0500

    completed delete flight functionality.

--- src/javascripts/components/cards/flightCard.js
+++ src/javascripts/components/cards/flightCard.js
@@ -1,30 +1,30 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import flightData from '../../helpers/data/flightData';
+import deleteFlight from '../../helpers/data/flightData';
 
 const flightCard = (flightObj) => {
   let buttons = '';
   const user = firebase.auth().currentUser;
   if (user) {
     buttons += `<a href='#' id='${flightObj.flightId}' class='btn btn-primary edit-flight'>Update Flight</a>
-    <a id='delete__${flightObj.flightId}' href='#' class='delete-flight btn btn-danger'>Delete Flight</a>`;
+    <a id='${flightObj.flightId}' href='#' class='delete-flight btn btn-danger'>Delete Flight</a>`;
   }
 
   const domString = $('#app').append(`
-    <div class="flight" id="flight-${flightObj.flightId}">
-    <div class="card" style="width: 18rem;">
+    <div class="flight card" id="flight-${flightObj.flightId}" style="width: 18rem;">
     <h3 class="card-title">Flight Number: ${flightObj.flightNumber}</h3>
     <div class="card-body">
       <h3 id="flight-departure-time">Departure Time: ${flightObj.departureTime}</h3>
       <h5 id="flight-duration">Flight Duration: ${flightObj.flightDuration}</h5>
-      ${buttons}
     </div>
+    ${buttons}
   </div>`);
+
   $('body').on('click', '.delete-flight', (e) => {
     e.stopImmediatePropagation();
-    const uid = e.currentTarget.id;
-    $(`.card#flight-${uid}`).remove();
-    flightData.deleteFlight(uid);
+    const flightId = e.currentTarget.id;
+    $(`.card#flight-${flightId}`).remove();
+    deleteFlight.deleteFlight(flightId);
   });
   return domString;
 };
