e1ab100967a9e1e3ad433a2d6b8c36f6802f9c21
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-24 10:23:52 -0500

    created function to create view to update flight.

---
+++ src/javascripts/components/views/updateFlightView.js
@@ -0,0 +1,11 @@
+import flightData from '../../helpers/data/flightData';
+import form from '../forms/crewUpdateForm';
+
+const updateFlight = (flightId) => {
+  $('#app').html('<div class="flightForm" id="update-flight"></div>');
+  flightData.getSingleFlight(flightId).then((response) => {
+    form.updateFlightForm(response);
+  });
+};
+
+export default { updateFlight };
