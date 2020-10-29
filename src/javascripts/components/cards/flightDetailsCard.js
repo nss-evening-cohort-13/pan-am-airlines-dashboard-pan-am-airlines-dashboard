ad5f949e481507b04b913df049cb5107bfddaa3b
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-10-28 22:28:12 -0500

    fixed it

--- src/javascripts/components/cards/flightDetailsCard.js
+++ src/javascripts/components/cards/flightDetailsCard.js
@@ -1,28 +1,21 @@
 import crewFunctions from '../../helpers/data/crewData';
 
-const getDataForDetailsCard = (id) => {
-  const flightCrew = crewFunctions.getFlightCrewByFlightId(id);
-  // const arrayCrew = [];
-  Promise.all([flightCrew]).then((values) => {
-    console.warn('each value', values);
-    // arrayCrew.push(values[key].name);
-    // // Object.keys(values).forEach((key) => {
-    // //   console.warn(key, values[key].name);
-    // //   arrayCrew.push(values[key].name);
-    // // });
-    return values;
-  });
-};
 const flightDetailsCard = (flightData) => {
-  const crew = getDataForDetailsCard(flightData.flightId);
-  console.warn('this is the crew:', crew);
   const domString = `
   <div class="flight-details-card" id="${flightData.flightId}">
       <div>${flightData.flightNumber}</div>
       <div>${flightData.flightDuration}</div>
       <div>${flightData.departureTime}</div>
-      <div>${crew}</div>
+      <h3> Crew: </h3>
+      <div id="daCrew"></div>
     </div>`;
+  crewFunctions.getFlightCrewByFlightId(flightData.flightId).then((response) => {
+    response.forEach((item) => {
+      $('#daCrew').append(
+        `<div class='${item.uid}'>${item.name}</div>`
+      );
+    });
+  });
   return domString;
 };
 
