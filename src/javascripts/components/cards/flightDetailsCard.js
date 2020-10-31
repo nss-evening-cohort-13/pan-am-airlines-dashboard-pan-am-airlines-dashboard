50075d37343a370d7dca3cc301203efc64d35d88
wkotheimer <wkotheimer@gmail.com>
2020-10-30 21:32:30 -0500

    Add flight complete to card

--- src/javascripts/components/cards/flightDetailsCard.js
+++ src/javascripts/components/cards/flightDetailsCard.js
@@ -10,21 +10,32 @@ const flightDetailsCard = (flightData) => {
       <div id="daCrew"></div>
       <h3> Pilot: </h3>
       <div id="pilot"></div>
+      <div id="complete"></div>
     </div>`;
   crewFunctions.getFlightCrewByFlightId(flightData.flightId).then((response) => {
     $('#pilot').html('');
     $('#daCrew').html('');
+    let pilots = 0;
+    let crew = 0;
     response.forEach((item) => {
       if (item.role === 'Pilot') {
+        pilots += 1;
+        crew += 1;
         $('#pilot').append(
           `<div class='${item.uid}'>${item.name}</div>`
         );
       } else {
+        crew += 1;
         $('#daCrew').append(
           `<div class='${item.uid}'>${item.name}</div>`
         );
       }
     });
+    if ((pilots >= 2) && (crew >= 4)) {
+      $('#complete').append('<h3 class="green">Complete Flight</h3>');
+    } else {
+      $('#complete').append('<h3 class="red">Incomplete Flight</h3>');
+    }
   });
   return domString;
 };
